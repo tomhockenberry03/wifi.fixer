a3285bf34ab0d69b5873349d8888fc32507eecd4
tomhockenberry <tomhockenberry03@gmail.com>
2021-03-16 13:48:22 -0400

    Now reads and interprets output of the ping command.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import subprocess
 
-#tests network connection by pinging google
-#if wifi is down, it will return the error "network is unreachable"
-subprocess.run("ping -c 1 8.8.8.8", shell=True)
+#defines p1 as the process of pinging google
+#for this process, -c 1 means ping it once, and 8.8.8.8 is the IP address of google.
+#in order to include multiple arguments, we need the shell=True.
+p1 = subprocess.run("ping -c 1 8.8.8.8", shell=True)
 
+#for a subprocess, it has a "returncode" that is a number indicating how many errors occur during the process.
+#defined result as the return code of process p1.
+result = p1.returncode
 
-subprocess.run("sudo service network-manager restart") #restarts the internet
+#if the the return code doesn't equal 0, meaning an error occured, reset the internet.
+if result != 0:
+    subprocess.run("sudo service network-manager restart") #restarts the internet
