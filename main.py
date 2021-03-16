161ff696f49d616f21e85de4e0dfc76d2641abe2
tomhockenberry <tomhockenberry03@gmail.com>
2021-03-16 08:43:44 -0400

    Add comments and add connection test

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+
+
 import os
 
-os.system("sudo service network-manager restart")
+
+os.system(cat < /dev/null > /dev/tcp/8.8.8.8/53; echo $?) #tests connection
+
+os.system("sudo service network-manager restart") #restarts the internet
