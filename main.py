33b2a19746577d6f9d087081e63501657f5f490d
tomhockenberry <tomhockenberry03@gmail.com>
2021-03-18 12:40:14 -0400

    Add larger time delay to allow network to properly reset before pinging again

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ def fixer(log):
     log.append(str(datetime.datetime.now()))
     subprocess.run("sudo service network-manager restart", shell=True)
     print("Fixed")
+    time.sleep(10)
     ping(result)
 
 def ping(result):
+    print(result)
     while result == 0:
         p1 = subprocess.run("ping -c 1 8.8.8.8", shell=True)
         print("Checked")
         result = p1.returncode
+        print(result)
         time.sleep(10)
     result = 0
     fixer(log)
