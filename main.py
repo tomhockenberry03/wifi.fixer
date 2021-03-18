569c2b1dbae0730c116dfffc1d0af7ef780f710c
tomhockenberry <tomhockenberry03@gmail.com>
2021-03-18 09:03:02 -0400

    Added loop

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import subprocess
 import datetime
 
-p1 = subprocess.run("ping -c 1 8.8.8.8", shell=True)
-result = p1.returncode
+def fix_and_continue(result):
+    if result != 0:
+        subprocess.run("sudo service network-manager restart", shell=True)
+        log = []
+        log.append(str(datetime.datetime.now()))
+    ping()
 
+def ping():
+    result = 0
+    while result == 0:
+        p1 = subprocess.run("ping -c 1 8.8.8.8", shell=True)
+        result = p1.returncode
+    fix_and_continue(result)
 
-if result != 0:
-    subprocess.run("sudo service network-manager restart", shell=True)
+ping()
 
-log=[]
-log.append(str(datetime.datetime.now()))
 
