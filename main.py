2afa58e117e63799e2740ff94f1d43a0801c96e1
tomhockenberry <tomhockenberry03@gmail.com>
2021-03-18 11:28:56 -0400

    Took away global definitions in functions.

--- main.py
+++ main.py
@@ -5,14 +5,10 @@ log = []
 result = 0
 
 def fixer(log):
-    global log
-    global result
     log.append(str(datetime.datetime.now()))
     subprocess.run("sudo service network-manager restart", shell=True)
 
 def ping(result):
-    global result
-    global log
     while result == 0:
         p1 = subprocess.run("ping -c 1 8.8.8.8", shell=True)
         result = p1.returncode
