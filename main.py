3ee9957f30aa57844935265e2386ef90df40eb8e
tomhockenberry <tomhockenberry03@gmail.com>
2021-03-18 11:36:32 -0400

    Loops are complete, program constantly scans network and fixes when necessary.

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ result = 0
 def fixer(log):
     log.append(str(datetime.datetime.now()))
     subprocess.run("sudo service network-manager restart", shell=True)
+    print("Fixed")
+    ping(result)
 
 def ping(result):
     while result == 0:
         p1 = subprocess.run("ping -c 1 8.8.8.8", shell=True)
+        print("Checked")
         result = p1.returncode
-        time.sleep(3)
+        time.sleep(10)
+    result = 0
     fixer(log)
 
 ping(result)
